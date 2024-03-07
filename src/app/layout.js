2e2c104a61d9d8447bb7816667cf8ad34cdaaa02
Kevin Camacho <Kevincxv@outlook.com>
2024-03-07 16:14:34 -0500

    Styles, Layouts, Containers

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='container'>
+          {children}
+          </div>
+      </body>
     </html>
   )
 }
