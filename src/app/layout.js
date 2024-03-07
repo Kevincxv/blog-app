cd6a11624be10ace0d499c4c2bfc11fe3c33308c
Kevin Camacho <Kevincxv@outlook.com>
2024-03-07 16:27:32 -0500

    CSS Responsive Layout Tutorial

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,21 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
-}
+  title: "Blog App",
+  description: "The best blog app!",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className='container'>
-          {children}
-          </div>
+        <div className="container">
+          <div className="wrapper">{children}</div>
+        </div>
       </body>
     </html>
-  )
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,13 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <Link href="/">
-      Hello
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis minima, nisi ullam voluptates earum sapiente libero assumenda error consequatur aliquam.</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti voluptas, at iste tenetur nostrum dolorum quod, inventore nemo reprehenderit aut ipsam est! Molestiae facere deleniti animi amet iste, odit, dolore placeat pariatur facilis non et! Quod ea culpa, ipsam provident iusto, ratione error necessitatibus, numquam voluptate harum omnis quidem voluptas?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti voluptas, at iste tenetur nostrum dolorum quod, inventore nemo reprehenderit aut ipsam est! Molestiae facere deleniti animi amet iste, odit, dolore placeat pariatur facilis non et! Quod ea culpa, ipsam provident iusto, ratione error necessitatibus, numquam voluptate harum omnis quidem voluptas?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti voluptas, at iste tenetur nostrum dolorum quod, inventore nemo reprehenderit aut ipsam est! Molestiae facere deleniti animi amet iste, odit, dolore placeat pariatur facilis non et! Quod ea culpa, ipsam provident iusto, ratione error necessitatibus, numquam voluptate harum omnis quidem voluptas?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti voluptas, at iste tenetur nostrum dolorum quod, inventore nemo reprehenderit aut ipsam est! Molestiae facere deleniti animi amet iste, odit, dolore placeat pariatur facilis non et! Quod ea culpa, ipsam provident iusto, ratione error necessitatibus, numquam voluptate harum omnis quidem voluptas?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti voluptas, at iste tenetur nostrum dolorum quod, inventore nemo reprehenderit aut ipsam est! Molestiae facere deleniti animi amet iste, odit, dolore placeat pariatur facilis non et! Quod ea culpa, ipsam provident iusto, ratione error necessitatibus, numquam voluptate harum omnis quidem voluptas?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti voluptas, at iste tenetur nostrum dolorum quod, inventore nemo reprehenderit aut ipsam est! Molestiae facere deleniti animi amet iste, odit, dolore placeat pariatur facilis non et! Quod ea culpa, ipsam provident iusto, ratione error necessitatibus, numquam voluptate harum omnis quidem voluptas?
     </Link>
   );
 }
