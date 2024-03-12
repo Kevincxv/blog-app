264a53fbb406d8e356c424185d9854906375e604
Kevin Camacho <Kevincxv@outlook.com>
2024-03-12 16:33:36 -0400

    Responsive Navbar Design

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import styles from "./navbar.module.css"
+import React from 'react';
+import styles from "./navbar.module.css";
+import Image from 'next/image';
+import Link from 'next/link';
+import AuthLinks from '../authLinks/AuthLinks';
+import ThemeToggle from '../themeToggle/ThemeToggle';
 
 const NavBar = () => {
   return (
     <div className={styles.container}>
-        NavBar
+        <div className={styles.social}>
+          <Image src="/facebook.png" alt="facebook" width={24} height={24} />
+          <Image src="/instagram.png" alt="instagram" width={24} height={24} />
+          <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
+          <Image src="/youtube.png" alt="youtube" width={24} height={24} />
+        </div>
+          <div className={styles.logo}>Culture-Blog</div>
+          <div className={styles.links}>
+          <ThemeToggle />
+            <Link href="/">HomePage</Link>
+            <Link href="/">Contact</Link>
+            <Link href="/">About</Link>
+            <AuthLinks />
+        </div>
     </div>
   )
 }
---
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './themeToggle.module.css'
+
+const ThemeToggle = () => {
+  return (
+    <div className={styles.container}>ThemeToggle</div>
+  )
+}
+
+export default ThemeToggle
