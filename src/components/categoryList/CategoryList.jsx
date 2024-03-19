9f8c617a3f47ffe432966846a7e7caac6d3168fb
Kevin Camacho <Kevincxv@outlook.com>
2024-03-19 15:55:04 -0400

    Category Component Design

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import styles from "./categoryList.module.css"
+import Link from "next/link"
+import Image from 'next/image'
 
 const categoryList = () => {
   return (
     <div className={styles.container}>
-        CategoryList
+        <h1 className={styles.title}>Popular Categories</h1>
+        <div className={styles.categories}>
+          <Link href="/blog" className={`${styles.category} ${styles.style}`}>
+            <Image src="/style.png" alt="" width={32} height={32} className={styles.image}></Image>
+            Style
+          </Link>
+          <Link href="/blog" className={`${styles.category} ${styles.style}`}>
+            <Image src="/fashion.png" alt="" width={32} height={32} className={styles.image}></Image>
+            Fashion
+          </Link>
+          <Link href="/blog" className={`${styles.category} ${styles.style}`}>
+            <Image src="/food.png" alt="" width={32} height={32} className={styles.image}></Image>
+            Food
+          </Link>
+          <Link href="/blog" className={`${styles.category} ${styles.style}`}>
+            <Image src="/travel.png" alt="" width={32} height={32} className={styles.image}></Image>
+            Travel
+          </Link>
+          <Link href="/blog" className={`${styles.category} ${styles.style}`}>
+            <Image src="/culture.png" alt="" width={32} height={32} className={styles.image}></Image>
+            Culture
+          </Link>
+          <Link href="/blog" className={`${styles.category} ${styles.style}`}>
+            <Image src="/coding.png" alt="" width={32} height={32} className={styles.image}></Image>
+            Coding
+          </Link>
+        </div>
     </div>
   )
 }
