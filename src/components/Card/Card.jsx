34c2630fc6fdd29a440ec2ed80f2b0c6cfe64c96
Kevin Camacho <Kevincxv@outlook.com>
2024-03-19 16:16:35 -0400

    Before CSS on Post Page

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,26 @@
+import styles from './card.module.css'
+import Image from "next/image"
+import Link from "next/link"
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.imageContainer}>
+            <Image src="/p1.jpg" alt="" fill className={styles.image}></Image>
+        </div>
+        <div className={styles.textContainer}>
+            <div className={styles.detail}>
+                <span className={styles.date}>03.19.2024</span>
+                <span className={styles.category}>CULTURE</span>
+            </div>
+            <Link href="/">
+                <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius doloremque ullam illo similique. Quod, non?</h1>
+            </Link>
+            <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sint harum laborum quia minima impedit dolor reprehenderit voluptatem officiis praesentium, velit ea quisquam reiciendis fugiat?</p>
+            <Link href="/" className={styles.link}>Read More</Link>
+        </div>
+    </div>
+  )
+}
+
+export default Card
--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import styles from "./cardList.module.css"
 import Pagination from '../pagination/Pagination'
+import Image from "next/image"
+import Card from '../Card/Card'
 
 const CardList = () => {
   return (
     <div className={styles.container}>
-        CardList
+        <h1 className={styles.title}>Recent Post</h1>
+        <div className={styles.posts}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
         <Pagination />
     </div>
   )
